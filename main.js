d85a0b1b47493b9072fafb193fb2bea4e7ef2ac2
EdilsonBispoCEO <98287629+EdilsonBispoCEO@users.noreply.github.com>
2023-02-23 08:46:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form-agenda');
+const numeros = [];
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+    
+    
+
+});
+
+function adicionaLinha() {
+    const inputNumero = document.getElementById('nome');
+    const inputNome = document.getElementById('numero');
+
+    if (numeros.includes(inputNumero.value)) {
+        alert(`O contato ${inputNumero.value} já foi inserida.`);
+    } else {
+    numeros.push(inputNumero.value) 
+
+    numeros.push(parseFloat(inputNome.value));
+    let linha = '<tr>';
+    linha += `<td>${inputNumero.value}</td>`;
+    linha += `<td>${inputNome.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+
+    inputNome.value = '';
+    inputNumero.value = '';
+    
+    }
+    
+}    
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
